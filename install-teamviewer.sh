eebb6e65dd76cd67fe255e6cf552f10d740237cc
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2014-05-02 15:13:29 -0500

    fixed typos

--- install-teamviewer.sh
+++ install-teamviewer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# Download the latest version of TeamViewer to te Downloads folder
+# Download the latest version of TeamViewer to the Downloads folder
 curl -o ~/Downloads/TeamViewer.dmg http://downloadus3.teamviewer.com/download/TeamViewer.dmg
 
 # Mount the .dmg
 hdiutil mount ~/Downloads/TeamViewer.dmg
 
-# Insall TeamViewer silentluy
+# Insall TeamViewer silently
 sudo /usr/sbin/installer -pkg /Volumes/TeamViewer/Install\ TeamViewer.pkg -target "/Volumes/Macintosh HD"
 
 # Unmount the volume when done
