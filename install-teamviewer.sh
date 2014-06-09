135d925e424094b42cbd65c004acfae26f6bf576
Jacob Salmela <jacobsalmela@users.noreply.github.com>
2014-06-09 13:44:41 -0500

    Update install-teamviewer.sh

--- install-teamviewer.sh
+++ install-teamviewer.sh
@@ -6,7 +6,12 @@ curl -o ~/Downloads/TeamViewer.dmg http://downloadus3.teamviewer.com/download/Te
 hdiutil mount ~/Downloads/TeamViewer.dmg
 
 # Insall TeamViewer silently
-sudo /usr/sbin/installer -pkg /Volumes/TeamViewer/Install\ TeamViewer.pkg -target "/Volumes/Macintosh HD"
+if [ $? = 0 ];then
+  sudo /usr/sbin/installer -pkg /Volumes/TeamViewer/Install\ TeamViewer.pkg -target "/Volumes/Macintosh HD"
+else
+  echo ".dmg not mounted."
+  exit 1
+fi
 
 # Unmount the volume when done
 umount /Volumes/TeamViewer
